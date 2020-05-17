85b3fcf43c3fd2e7ad433f3acbd6cb5233bb2796
Nathanael Weber <nwp8nice@rack.farm>
2020-05-17 11:24:45 +0200

    fix: collide function

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ import time
 import numpy
 
 
-def check_on_line(A, B, S):
-    if A.x < B.x and A.y < B.y:
-        return A.x <= S.x <= B.x and A.y <= S.y <= B.y
-    elif A.x < B.x and A.y > B.y:
-        return A.x <= S.x <= B.x and A.y >= S.y >= B.y
-    elif A.x >= B.x and A.y < B.y:
-        return A.x >= S.x >= B.x and A.y <= S.y <= B.y
-    else:
-        return A.x >= S.x >= B.x and A.y >= S.y >= B.y
+def check_collision(s):
+    return 1 >= s > 0
 
 
-def has_collision(point, vector, wall):
+def collide(point, vector, wall):
     A = wall.from_point
     B = wall.to_point
     v = vector
@@ -30,16 +23,13 @@ def has_collision(point, vector, wall):
         P.y - A.y
     ])
 
-    print(equation)
-    print(solution)
+    solved = numpy.linalg.solve(equation, solution)
+    t = solved[0]
+    s = solved[1]
 
-    point = numpy.linalg.solve(equation, solution)
+    S = numpy.array([P.x, P.y]) + s * numpy.array([v["x"], v["y"]])
 
-    print(point)
-
-    S = Point({"x": point[0], "y": point[1]})
-
-    return check_on_line(A, B, S)
+    return {"point": Point({"x": S[0], "y": S[1]}), "s": s, "t": t}
 
 
 def mirror(point_p, point_s):
@@ -116,14 +106,11 @@ class Billard(Canvas):
     ball_point = None
     temp_point = None
 
-
     ball = None
-
 
     def mirror(self, point, wall):
         print(wall)
         print(point)
-
 
     def left_click(self, event):
         print("left click at", event.x, event.y)
@@ -134,25 +121,19 @@ class Billard(Canvas):
         else:
             self.add_wall(self.temp_point, Point({"x": event.x, "y": event.y}))
 
-
-        
-
     def cutpoint(self, a, b):
-        m_a  = a.x / a.y
+        m_a = a.x / a.y
         c_a = a.y - a.x * m_a
 
-        m_b  = b.x / b.y
+        m_b = b.x / b.y
         c_b = b.y - b.x * m_b
 
-        x = (m_b - m_a)/(c_b - c_a)
+        x = (m_b - m_a) / (c_b - c_a)
 
         return Point({
             'x': x,
             'y': x * m_a + c_a,
         })
-
-
-
 
     def right_click(self, event):
         print("right click at", event.x, event.y)
@@ -166,10 +147,8 @@ class Billard(Canvas):
             point.sub(self.ball_point)
             point.sub(self.ball_point)
 
-
             m = point.factor()
             cutpoint()
-
 
             # todo we have to define how many steps we want to loop througth
             # a possible solution can be the diagonal length of the screen -> the ball will never run longer than that for one line
@@ -221,7 +200,7 @@ class Billard(Canvas):
 
 
 print(
-    has_collision(Point({"x": 6, "y": 6}), {"x": 1, "y": 0}, Wall(Point({"x": 10, "y": 0}), Point({"x": 20, "y": 20})))
+    mirror(Point({"x": 6, "y": 6}), collide(Point({"x": 6, "y": 6}), {"x": 1, "y": 0}, Wall(Point({"x": 10, "y": 0}), Point({"x": 20, "y": 20})))["point"])
 )
 # root = Tk()
 # root.geometry("500x500")
